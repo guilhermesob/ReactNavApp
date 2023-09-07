9b3a7cb8d80a588746c5adc32a6e7ff208eceafe
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-09-07 20:21:00 -0300

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { FC } from 'react';
 
 import './style.css';
 
+
+
 export const App: FC<{ name: string }> = ({ name }) => {
   return (
     <div>
       <h1>Hello {name}!</h1>
-      <p>Start editing to see some magic happen in thi worrld :)</p>
+      <p>Start editing to see some magic happen in this world :)</p>
     </div>
   );
 };
